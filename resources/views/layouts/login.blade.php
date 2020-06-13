5c83644556c17148e83546a72defa90f67dbe501
Ravina <riyamaniya29@gmal.com>
2020-06-13 08:25:55 +0530

    ghgf

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -7,19 +7,19 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>OCR | @yield('title')</title>
   <!-- plugins:css -->
-  <link rel="stylesheet" href="{{asset('assets/vendors/iconfonts/mdi/css/materialdesignicons.min.css')}}">
-  <link rel="stylesheet" href="{{asset('assets/vendors/iconfonts/flag-icon-css/css/flag-icon.min.css')}}">
-  <link rel="stylesheet" href="{{asset('assets/vendors/iconfonts/simple-line-icon/css/simple-line-icons.css')}}">
-  <link rel="stylesheet" href="{{asset('assets/vendors/css/vendor.bundle.base.css')}}">
-  <link rel="stylesheet" href="{{asset('assets/vendors/css/vendor.bundle.addons.css')}}">
+  <link rel="stylesheet" href="{{asset('public/assets/vendors/iconfonts/mdi/css/materialdesignicons.min.css')}}">
+  <link rel="stylesheet" href="{{asset('public/assets/vendors/iconfonts/flag-icon-css/css/flag-icon.min.css')}}">
+  <link rel="stylesheet" href="{{asset('public/assets/vendors/iconfonts/simple-line-icon/css/simple-line-icons.css')}}">
+  <link rel="stylesheet" href="{{asset('public/assets/vendors/css/vendor.bundle.base.css')}}">
+  <link rel="stylesheet" href="{{asset('public/assets/vendors/css/vendor.bundle.addons.css')}}">
   <!-- endinject -->
   <!-- plugin css for this page -->
   <!-- End plugin css for this page -->
   <!-- inject:css -->
-  <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+  <link rel="stylesheet" href="{{asset('public/assets/css/style.css')}}">
   <!-- endinject -->
-  <link rel="shortcut icon" href="{{asset('assets/images/favicon.png')}}" />
-  <script src="{{asset('/vendor/jquery/jquery.min.js')}}"></script>
+  <link rel="shortcut icon" href="{{asset('public/assets/images/favicon.png')}}" />
+  <script src="{{asset('public/vendor/jquery/jquery.min.js')}}"></script>
   @yield('css')
 </head>
 
@@ -35,21 +35,21 @@
   </div>
   <!-- container-scroller -->
   <!-- plugins:js -->
-  <script src="{{ asset('assets/vendors/js/vendor.bundle.base.js')}}"></script>
-  <script src="{{ asset('assets/vendors/js/vendor.bundle.addons.js')}}"></script>
+  <script src="{{ asset('public/assets/vendors/js/vendor.bundle.base.js')}}"></script>
+  <script src="{{ asset('public/assets/vendors/js/vendor.bundle.addons.js')}}"></script>
   <!-- endinject -->
   <!-- inject:js -->
-  <script src="{{ asset('assets/js/off-canvas.js')}}"></script>
-  <script src="{{ asset('assets/js/hoverable-collapse.js')}}"></script>
-  <script src="{{ asset('assets/js/misc.js')}}"></script>
-  <script src="{{ asset('assets/js/settings.js')}}"></script>
-  <script src="{{ asset('assets/js/todolist.js')}}"></script>
-   <script src="{{asset('vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+  <script src="{{ asset('public/assets/js/off-canvas.js')}}"></script>
+  <script src="{{ asset('public/assets/js/hoverable-collapse.js')}}"></script>
+  <script src="{{ asset('public/assets/js/misc.js')}}"></script>
+  <script src="{{ asset('public/assets/js/settings.js')}}"></script>
+  <script src="{{ asset('public/assets/js/todolist.js')}}"></script>
+   <script src="{{asset('public/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
 
   <!-- Custom JavaScript for this theme -->
-  <script src="{{asset('js/scrolling-nav.js')}}"></script>
-  <script src="{{asset('js/jquery.maskedinput.js')}}"></script>
-  <script src="{{asset('js/jquery-ui.js')}}"></script>
+  <script src="{{asset('public/js/scrolling-nav.js')}}"></script>
+  <script src="{{asset('public/js/jquery.maskedinput.js')}}"></script>
+  <script src="{{asset('public/js/jquery-ui.js')}}"></script>
   <!-- endinject -->
   @yield('js')
 </body>
