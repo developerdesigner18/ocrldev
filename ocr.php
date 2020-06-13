75e17caf9a65e64bfe4a1e81aa20dd2307ce28aa
Ravina <riyamaniya29@gmal.com>
2020-06-13 11:14:30 +0530

    gfgf

--- ocr.php
+++ ocr.php
@@ -2,8 +2,8 @@
 use thiagoalessio\TesseractOCR\TesseractOCR;
 
 $image_file = 'https://ocrldev.herokuapp.com/public/photos/6141591615254.png';
-// dd($image_file);
-echo (new TesseractOCR($image_file))->command;
+dd($image_file);
+echo (new TesseractOCR($image_file))->run();
 
 
 ?>
