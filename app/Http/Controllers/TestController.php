d494485768a79e58eb33311814380961948e7251
Ravina <riyamaniya29@gmal.com>
2020-06-13 11:48:50 +0530

    gfh

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use thiagoalessio\TesseractOCR\TesseractOCR;
 class TestController extends Controller
 {
     public function test()
     {
-    	echo "test";
+    	$image_file = 'https://ocrldev.herokuapp.com/public/photos/6141591615254.png';
+		dd($image_file);
+		echo (new TesseractOCR($image_file))->run();
     }
 }
