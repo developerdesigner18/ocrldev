14019ec05d1349860eddd833300504d2dbe5a94c
Ravina <riyamaniya29@gmal.com>
2020-06-13 09:48:56 +0530

    hfgh

--- ocr.php
+++ ocr.php
@@ -1,10 +1,9 @@
 <?php
 
-use thiagoalessio\TesseractOCR\TesseractOCR;
+
 
 $image_file = public_path().'/photos/361591415735.jpg';
     dd($image_file);
-echo (new TesseractOCR('/public/photos/6141591615254.png'))
-->run();
+
 
 ?>
