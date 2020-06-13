f376632d68c3ff3652558d68121f854bf7096f9d
Ravina <riyamaniya29@gmal.com>
2020-06-13 09:59:38 +0530

    ghg

--- ocr.php
+++ ocr.php
@@ -1,8 +1,9 @@
 <?php
+use thiagoalessio\TesseractOCR\TesseractOCR;
 
 $image_file = 'https://ocrldev.herokuapp.com/public/photos/361591415735.jpg';
 
-echo $image_file;
+echo (new TesseractOCR($image_file))->run();
 
 
 ?>
