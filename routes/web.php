78659e10d3de289caee970c6c298c5a8ff38d6e1
Ravina <riyamaniya29@gmal.com>
2020-06-13 11:45:43 +0530

    fgdf

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/api', 'HomeController@test')->name('api');
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
-Route::get('/api', 'HomeController@test')->name('api');
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['prefix' => 'admin'], function () {
