d8968679cc6cba3c4ea546f02c51cf176138a64a
Ravina <riyamaniya29@gmal.com>
2020-06-13 09:44:30 +0530

    df

--- ocr.php
+++ ocr.php
@@ -1,9 +1,6 @@
 <?php
 // require_once __DIR__ . '/vendor/autoload.php';
-dd('++');
-use thiagoalessio\TesseractOCR\TesseractOCR;
+echo "hii";
 
-$image_file = public_path().'/photos/'.$record['path'];
-    dd($image_file);
-echo (new TesseractOCR('/public/photos/6141591615254.png'))
-->run();
+
+?>
