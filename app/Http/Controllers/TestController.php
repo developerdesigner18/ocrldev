67142e4876791c5fc38c87af68b9e70821252ea7
Ravina <riyamaniya29@gmal.com>
2020-06-15 11:59:44 +0530

    sasdas

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,7 +8,7 @@ class TestController extends Controller
 {
     public function test()
     {
-    	echo $image_file = public_path().'photos/6141591615254.png';
+    	echo $image_file = public_path().'/photos/6141591615254.png';
     	echo "<br>";
 
 		// dd($image_file);
