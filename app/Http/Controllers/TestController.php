8f86d80adf97ef0a847c492aa87c546c38135fea
Ravina <riyamaniya29@gmal.com>
2020-06-15 16:20:23 +0530

    rpp

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -9,6 +9,8 @@ class TestController extends Controller
     public function test()
     {
     	$image_file="https://www.ocrldev.xyz/photos/8911591423706.jpg";
+    	echo $image_file;
+    	die;
     	// $image_file = public_path().'/photos/7631591416660.jpg';
 		$result=(new TesseractOCR($image_file))->run();
 		echo $result;
