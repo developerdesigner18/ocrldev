dbedd76cea8b3b08412931f025899367edea8e49
Ravina <riyamaniya29@gmal.com>
2020-06-15 17:56:28 +0530

    hty

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -17,8 +17,8 @@ class TestController extends Controller
         $image=$request->file('photo');
     	$image_name = rand(1, 999).time().'.'.$image->getClientOriginalExtension();
         $image->move(public_path().'/photos/',$image_name);
-    	die;
-  //   	$image_file = public_path().'/photos/7631591416660.jpg';
+ 
+     	$image_file = public_path().'/photos/'.$image_name;
     	
 		// $result=(new TesseractOCR($image_file))->run();
 		// echo $result;
@@ -27,10 +27,10 @@ class TestController extends Controller
 		// $image_="8911591423706.jpg";
     	// $img=$request->photo->move(public_path('/photos'));
     	// $image_file = public_path().'/photos/'.$img;
-    	echo $image;
+    	echo $image_file;
 
     	// $image_file = public_path().'/photos/7631591416660.jpg';
-		$result=(new TesseractOCR($image))->run();
+		$result=(new TesseractOCR($image_file))->run();
 		echo $result;
 		die;
     }
