0357f3cc9e57eb2114e43df7e91530ce1cb5509d
Ravina <riyamaniya29@gmal.com>
2020-06-15 18:13:24 +0530

    hjh

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -12,26 +12,13 @@ class TestController extends Controller
 	}
     public function test(Request $request)
     {
-
-
-        $image='7631591416660.jpg';
-    	// $image_name = rand(1, 999).time().'.'.$image->getClientOriginalExtension();
-        // $image->move(public_path().'/photos/',$image_name);
- 
+        $image=$request->image;
     	move_uploaded_file($image, public_path().'/photos/'.$image);
-		// $result=(new TesseractOCR($image_file))->run();
-		// echo $result;
      	$image_file = public_path().'/photos/'.$image;
-
-
-		// $image_="8911591423706.jpg";
-    	// $img=$request->photo->move(public_path('/photos'));
-    	// $image_file = public_path().'/photos/'.$img;
-    	echo $image_file;
 
     	// $image_file = public_path().'/photos/7631591416660.jpg';
 		$result=(new TesseractOCR($image_file))->run();
 		echo $result;
-		die;
+		
     }
 }
