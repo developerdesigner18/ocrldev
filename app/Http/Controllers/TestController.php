4e16716bd42563be04124710fc133cf7c9b27355
Ravina <riyamaniya29@gmal.com>
2020-06-16 12:47:43 +0530

    gfhfgh

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -16,19 +16,19 @@ class TestController extends Controller
         $image=$request->image;
         // $image='8911591423706.jpg';
         $url = 'https://www.ocrldev.xyz/photos/'.$image;
-        // echo $url;
+        echo $url;
         $img =  public_path().'/photos/'.$image;
         file_put_contents($img, file_get_contents($url));
-        // echo "<br>";
-     //    echo $image;
+        echo "<br>";
+        echo $image;
     	// move_uploaded_file($image, public_path().'/photos/'.$image);
      	$image_file = public_path().'/photos/'.$image;
-        // echo $image_file;
+        echo $image_file;
         // echo "<br>";
     	// $image_file = public_path().'/photos/7631591416660.jpg';
 		$result=(new TesseractOCR($image_file))->run();
-        // echo $result;
-        // die;
+        echo $result;
+        die;
 		
         return $result;
 		
