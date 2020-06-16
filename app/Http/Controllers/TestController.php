67e82a1656a33f6d1b843e3393a7a7a8a2d8b85b
Ravina <riyamaniya29@gmal.com>
2020-06-16 12:40:58 +0530

    yty

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -19,14 +19,16 @@ class TestController extends Controller
         echo $url;
         $img =  public_path().'/photos/'.$image;
         file_put_contents($img, file_get_contents($url));
-
+        echo "<br>";
      //    echo $image;
     	// move_uploaded_file($image, public_path().'/photos/'.$image);
      	$image_file = public_path().'/photos/'.$image;
         echo $image_file;
-        die;
+        echo "<br>";
     	// $image_file = public_path().'/photos/7631591416660.jpg';
 		$result=(new TesseractOCR($image_file))->run();
+        echo $result;
+        die;
 		
         return $result;
 		
