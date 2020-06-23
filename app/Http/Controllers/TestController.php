82e308c35026d51db4df8efc8c77f11e41fe10cd
Ravina <riyamaniya29@gmal.com>
2020-06-23 17:19:12 +0530

    ghfgh

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -18,7 +18,7 @@ class TestController extends Controller
         // $image = Input::get('image');
         // $image=$request->file('image');
         $image=$request->image;
-        echo $image;
+        // echo $image;
         // $image='7631591416660.jpg';
 
 
@@ -26,14 +26,14 @@ class TestController extends Controller
         // $img =  public_path().'/photos/'.$image;
 
         $url = 'https://www.ocrldev.xyz/'.$image;
-        echo $url;
+        // echo $url;
         $img =  public_path().'/'.$image;
 
         file_put_contents($img, file_get_contents($url));
 
        
      	$image_file = public_path().'/'.$image;
-        echo $image_file;
+        // echo $image_file;
 		$result=(new TesseractOCR($image_file))->whitelist(range(0, 9),range('A', 'Z'))->run();
         // echo $result;
         // die;
