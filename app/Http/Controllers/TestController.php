dc01e44a5e21bf18cb483666c177c9cfaa190012
Ravina <riyamaniya29@gmal.com>
2020-06-23 17:08:27 +0530

    gfd

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -21,16 +21,18 @@ class TestController extends Controller
         echo $image;
         // $image='7631591416660.jpg';
 
-        
-        // $url='http://127.0.0.1:8000/photos/'.$image;
 
-        $url = 'https://www.ocrldev.xyz/photos/'.$image;
-        $img =  public_path().'/photos/'.$image;
+        // $url = 'https://www.ocrldev.xyz/photos/'.$image;
+        // $img =  public_path().'/photos/'.$image;
+
+        $url = 'https://www.ocrldev.xyz/'.$image;
+        $img =  public_path().'/'.$image;
+
         file_put_contents($img, file_get_contents($url));
 
        
      	$image_file = public_path().'/photos/'.$image;
-        // echo $image_file;
+        echo $image_file;
 		$result=(new TesseractOCR($image_file))->whitelist(range(0, 9),range('A', 'Z'))->run();
         // echo $result;
         // die;
